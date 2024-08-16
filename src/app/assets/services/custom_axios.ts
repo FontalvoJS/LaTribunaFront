eee30fdd765802f3768012e8755da4d83d12796e
Fontalvojs <mejia_andres@hotmail.es>
2024-08-16 01:10:13 -0500

    Intento de arreglo del axios personalizado #1

--- src/app/assets/services/custom_axios.ts
+++ src/app/assets/services/custom_axios.ts
@@ -1,8 +1,7 @@
-'use client'
 import axios from "axios";
 
 export const custom_axios = axios.create({
-  baseURL: "http://localhost:3000/api/", // Cambia esto a tu URL base
+  baseURL: "localhost/api/", // Cambia esto a tu URL base
   timeout: 10000,
   withCredentials: true,
 });
