e37f72bc2cd9f9587975e2e40df9d778a3715069
Fontalvojs <mejia_andres@hotmail.es>
2024-08-16 10:38:47 -0500

    Intento de arreglo del axios personalizado #4

--- src/app/assets/services/custom_axios.ts
+++ src/app/assets/services/custom_axios.ts
@@ -1,8 +1,17 @@
 'use client';
 import axios from "axios";
 
+let baseURL = "";
+
+if (typeof window !== "undefined") {
+  baseURL = window.location.origin + "/api/";
+} else {
+  // Puedes definir una URL base alternativa para el entorno del servidor si es necesario
+  baseURL = "http://localhost/api/";
+}
+
 export const custom_axios = axios.create({
-  baseURL: "https://la-tribuna-front.vercel.app", // Cambia esto a tu URL base
+  baseURL: baseURL, // URL base dinámica
   timeout: 10000,
   withCredentials: true,
 });
